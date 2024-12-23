8652f539d5ac50fd0aaf089610236376335c8be2
Lauren R. Kinder <elopii4002@gmail.com>
2024-12-23 11:54:54 +0000

    started work on the GUI (pygame) word processor

--- components/tinyifier.py
+++ components/tinyifier.py
@@ -15,14 +15,14 @@ def tinyify(text, BANKS, ALLCHARS, CODES=CODES, debug=False):
     def getvaluesforchar(char, currentbank):
         if char.isdigit():
             return [format(int(char), "04b")], currentbank  # if the character is a digit, return the 4 bit binary representation of the digit and the current bank
-        char = char.lower()                                 # make the character lowercase (we've already checked for caps at this point)
+        char = char.lower()                                 # make the character lowercase to match the banks (we've already checked for caps at this point)
         foundbank = None
         for i, bank in enumerate(BANKS):
             if char in bank:
                 foundbank = i                           # locate the bank that contains the desired character
                 break                                   # stop looking once we've found it
         if foundbank is None:                           # this should not happen but if it does it's not a big deal,
-            return []                                   # we can just return an empty list
+            return [], currentbank                      # we can just return an empty list and the current bank (we aren't going to move)
         steps = (foundbank - currentbank) % len(BANKS)  # calculate the number of bank switches needed to reach desired bank
         output = ["1111"] * steps                       # add a corresponding amount of switch codes to the output values
         charindex = BANKS[foundbank].index(char)        # get the index number of the char within the bank
@@ -30,7 +30,7 @@ def tinyify(text, BANKS, ALLCHARS, CODES=CODES, debug=False):
         output.append(charindex)                        # finally add char value to the output (if no switches are needed, this will be the only value)
         return output, foundbank                        # return the output values and the bank the character was found in (so we can keep track of it)
     
-    def converttobin(tiniedtext, beforelength):
+    def converttobin(tiniedtext, beforelength, debug=False):
         filelength = len(tiniedtext)                                            # get the length of the converted nibble list
         if filelength % 2 != 0:                                                 # if there is an odd number of nibbles we should make it even so we can convert to bytes
             tiniedtext.append("0000")                                           # add a 0 nibble to the end of the file
@@ -41,10 +41,12 @@ def tinyify(text, BANKS, ALLCHARS, CODES=CODES, debug=False):
         for i in range(0, len(tiniedtext)-1, 2):                                    # iterate through the file in pairs
             byte = tiniedtext[i] + tiniedtext[i + 1]                                # combine two nibbles into a byte
             byte = int(byte, 2)                                                     # convert the byte to an integer
-            if byte > 255:                                                          # if the byte is greater than 255, something went wrong
-                print("ERROR: byte too big for he got damn feet") 
-            filebytes.append(byte)                                                  # add the byte to the list of bytes
-        print(f"File reduced by {(beforelength * 8) - (len(filebytes) * 8)} bits")  # brag about how much we reduced the file
+            if byte > 255:                                                                  # if the byte is above 255, something went wrong
+                print(f"ERROR: byte exceeds 255 somehow, value: {byte}, capping at 255 ({byte % 256})")    # print an error message and move on
+                byte = byte % 256                                                           # cap the byte at 255, hopefully by doing modulo it can recover whatever messed up garbage byte it was
+            filebytes.append(byte)  # add the byte to the list of bytes
+        if debug:                                                                       
+            print(f"File reduced by {(beforelength * 8) - (len(filebytes) * 8)} bits")  # brag about how much we reduced the file
         return filebytes
     
     beforelength = len(text)
@@ -53,11 +55,10 @@ def tinyify(text, BANKS, ALLCHARS, CODES=CODES, debug=False):
     charmode = "lower"          # start in lowercase mode
 
     for i, char in enumerate(text):
-        print(char)
         charvalues = []
         if char.lower() not in ALLCHARS:
             continue
-        if char.isupper() and not charmode == "upper":
+        if char.isupper() and charmode != "upper":
             nextlastcap = getnextlastcap(text, i)
             if nextlastcap != -1:
                 if (nextlastcap - i) > 1:                               # if there is more than one capital letter in a row, switch to upper mode
@@ -69,11 +70,11 @@ def tinyify(text, BANKS, ALLCHARS, CODES=CODES, debug=False):
                     modeindex = format(CODES.index(charmode), "04b")
                     charvalues.extend(["1110", modeindex])
                     charmode = "lower"                                  # switch back to lowercase after onecap mode
-        elif char.islower() and not charmode == "lower":
+        elif char.islower() and charmode != "lower":
             charmode = "lower"                                          # switching to lowercase if needed
             modeindex = format(CODES.index(charmode), "04b")
             charvalues.extend(["1110", modeindex])
-        elif char.isdigit() and not charmode == "digit":
+        elif char.isdigit() and charmode != "digit":
             charmode = "digit"                                          # switching to digit mode if needed
             modeindex = format(CODES.index(charmode), "04b")
             charvalues.extend(["1110", modeindex])
@@ -81,15 +82,6 @@ def tinyify(text, BANKS, ALLCHARS, CODES=CODES, debug=False):
         newvalues, currentbank = getvaluesforchar(char, currentbank)                                  
         charvalues.extend(newvalues)
         tiniedtext.extend(charvalues)
-    bintext = converttobin(tiniedtext, beforelength)
+    bintext = converttobin(tiniedtext, beforelength, debug=debug)
     afterlength = len(bintext)
     return bintext, beforelength, afterlength
-        
-# if __name__ == "__main__":
-#     from components.bigifier import bigify
-#     sampletext = ""
-#     tinyified, beforelength, afterlength = tinyify(sampletext)
-#     bigified = bigify(tinyified)
-#     print(tinyified)
-#     print(bigified)
-#     print(f"Before: {beforelength} After: {afterlength}")
--- main.py
+++ main.py
@@ -13,4 +13,65 @@ CODES = [
 
 from components import bigify
 from components import tinyify
+import pygame
+import sys
 
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("TinyLETTERS")
+
+fontname = pygame.font.match_font("consolas")
+fontsize = 25
+font = pygame.font.Font(fontname, fontsize)
+linepadding = 4
+white = (255, 255, 255)
+grey = (128, 128, 128)
+black = (0, 0, 0)
+cursortimer = 0
+cursorvisible = True
+pygame.key.set_repeat(300, 50)
+
+
+bodytext = ["test", "test2", "test3"]
+bodytextsurf = None
+cursorvisible = True
+
+running = True
+clock = pygame.time.Clock()
+while running:
+    clock.tick(60)                                  # limit the frame rate to 60 FPS
+
+    cursortimer += 1                                # increment the cursor timer once per frame
+    if cursortimer > 30:                            # every 30 frames (0.5 seconds)
+        cursorvisible = not cursorvisible           # toggle the cursor visibility
+        cursortimer = 0                             # reset the timer
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:            # on enter
+                bodytext.append("")                     # add a new line to the body text list
+            elif event.key == pygame.K_BACKSPACE:                                   # on backspace
+                if bodytext[len(bodytext)-1] != "":                                 # if the line has stuff in it
+                    bodytext[len(bodytext)-1] = bodytext[len(bodytext)-1][:-1]      # remove the last character
+                else:                                                               # if the line is empty
+                    bodytext.pop()                                                  # just remove the line from the list entirely
+            else:                                                                   # if any other key is pressed
+                bodytext[len(bodytext)-1] += event.unicode                          # add the keyboard input to the last line (may result in destructive results)
+
+    window.fill(black)
+
+    lineheight = 50
+    for line in bodytext:
+        bodytextsurf = font.render(line, True, white)
+        window.blit(bodytextsurf, (100, lineheight))
+        lineheight += fontsize + linepadding
+
+
+    pygame.display.flip()
+    
+        
